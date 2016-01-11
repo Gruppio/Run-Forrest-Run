819edf55d2adea74b90f1a64ebe12e5921809041
Michele Gruppioni <gruppiofigo@hotmail.it>
2016-01-12 00:41:26 +0100

    Initial Commit

---
+++ Run-Forrest-Run/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Run-Forrest-Run
+//
+//  Created by Michele Gruppioni on 12/01/16.
+//  Copyright © 2016 Michele Gruppioni. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
